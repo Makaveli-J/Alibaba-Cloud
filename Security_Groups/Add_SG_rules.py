51e4d0ee3bdf8720fd5b1bae4dd4b902af8759c8
Makaveli-J <544301790@qq.com>
2021-10-13 13:14:07 +0800

    update

--- Security_Groups/Add_SG_rules.py
+++ Security_Groups/Add_SG_rules.py
@@ -12,18 +12,18 @@ client = AcsClient(os.environ['ACCESS_KEY_ID'], os.environ['ACCESS_KEY_SECRET'],
 request = AuthorizeSecurityGroupRequest()
 request.set_accept_format('json')
 
-request.set_IpProtocol("tcp")
-request.set_SourceCidrIp("10.0.0.0/8")
-request.set_Policy("accept")
-request.set_Priority("1")
-request.set_Description("test")
+request.set_IpProtocol('tcp')
+request.set_SourceCidrIp('10.0.0.0/8')
+request.set_Policy('accept')
+request.set_Priority('1')
+request.set_Description('test')
 
 #Security Group List
-sg_lst = ['sg-test','sg-test0']
-lst_p = ['20/22','80/80','443/443']
+sg_lst = ['sg-test', 'sg-test0']
+lst_p = ['20/22', '80/80', '443/443']
 
 #For Loop to add rules
-for i in lst:
+for i in sg_lst:
     for p in lst_p:
         request.set_PortRange(p)
         request.set_SecurityGroupId(i)
--- Security_Groups/Delete_SG_rules.py
+++ Security_Groups/Delete_SG_rules.py
@@ -12,15 +12,16 @@ client = AcsClient(os.environ['ACCESS_KEY_ID'], os.environ['ACCESS_KEY_SECRET'],
 request = RevokeSecurityGroupRequest()
 request.set_accept_format('json')
 
-request.set_IpProtocol("tcp")
-request.set_SourceCidrIp("10.0.0.0/8")
-request.set_Description("test")
+request.set_IpProtocol('tcp')
+request.set_SourceCidrIp('10.0.0.0/8')
+request.set_Description('test')
 
-sg_lst = ['sg-test','sg-test0']
-lst_p = ['20/22','80/80','443/443']
+#Security Group List
+sg_lst = ['sg-test', 'sg-test0']
+lst_p = ['20/22', '80/80', '443/443']
 
 #For Loop to delete rules
-for i in lst:
+for i in sg_lst:
     for p in lst_p:
         request.set_PortRange(p)
         request.set_SecurityGroupId(i)
