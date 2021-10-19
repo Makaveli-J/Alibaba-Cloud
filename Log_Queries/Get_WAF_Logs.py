ba40f1b48813f5efd951387fc5c10ac67c8ed6d2
Makaveli-J <544301790@qq.com>
2021-10-19 16:35:11 +0800

    Update Get_WAF_Logs.py

--- Log_Queries/Get_WAF_Logs.py
+++ Log_Queries/Get_WAF_Logs.py
@@ -5,6 +5,7 @@ from aliyun.log import LogClient, LogItem, GetLogsRequest, IndexConfig
 import time
 import os
 import Set_Time
+import Validate_Selection
 
 # set time range for the log query
 time_range = Set_Time.get_time()
@@ -13,10 +14,10 @@ from_time = time_range[0]
 to_time = time_range[1]
 
 # define WAF log queries
-query1 = '* | SELECT count(*) AS ctr, date_trunc(\'day\', __time__) AS date0 GROUP BY date0 ORDER BY date0 ASC | FROM'
-query2 = '{* and status: 405} | SELECT count(*) AS ctr, date_trunc(\'day\', __time__) AS date0 GROUP BY date0 ORDER BY date0 ASC | FROM'
-query3 = '* | SELECT real_client_ip, count(*) AS ctr GROUP BY real_client_ip ORDER BY ctr DESC LIMIT 10'
-query4 = '{* and status: 405} | SELECT real_client_ip, count(*) AS ctr GROUP BY real_client_ip ORDER BY ctr DESC LIMIT 10'
+query1 = '* | SELECT count(*) AS ctr, date_trunc(\'day\', __time__) AS date0 GROUP BY date0 ORDER BY date0 ASC | FROM '
+query2 = '{* and status: 405} | SELECT count(*) AS ctr, date_trunc(\'day\', __time__) AS date0 GROUP BY date0 ORDER BY date0 ASC | FROM '
+query3 = '* | SELECT real_client_ip, count(*) AS ctr GROUP BY real_client_ip ORDER BY ctr DESC LIMIT 10 | FROM '
+query4 = '{* and status: 405} | SELECT real_client_ip, count(*) AS ctr GROUP BY real_client_ip ORDER BY ctr DESC LIMIT 10 | FROM '
 custom_query = ''
 
 query_selection = {1: query1, 2: query2, 3: query3, 4: query4, 5: custom_query}
@@ -28,7 +29,14 @@ print('2. Daily Blocked Requests')
 print('3. Top 10 Source IP')
 print('4. Top 10 Blocked Source IP')
 print('5. Custom Query')
-choice_made = int(input())
+choice_made = input()
+
+first_option = 1
+last_option = 6
+
+while Validate_Selection.validate_selection(choice_made, first_option, last_option) is False:
+    choice_made = input('Invalid input, please try again\n')
+    Validate_Selection.validate_selection(choice_made, first_option, last_option)
 
 client = LogClient('cn-hangzhou.log.aliyuncs.com', os.environ['ACCESS_KEY_ID'], os.environ['ACCESS_KEY_SECRET'])
 
@@ -36,7 +44,11 @@ project_name = 'waf-project-test-cn-hangzhou'
 
 logstore_name = 'waf-test-logstore'
 
-query = query_selection.get(choice_made) + logstore_name
+if choice_made in range(1, 5):
+    query = query_selection.get(choice_made) + logstore_name
+else:
+    query = input('Please enter custom query:\n') + ' | FROM ' + logstore_name
+
 
 def get_logs():
     print("ready to query logs from logstore %s" % logstore_name)
